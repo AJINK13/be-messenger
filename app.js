e552a810f865a9f231124c1042a699d65f7361a0
aj <ajshanthappa@gmail.com>
2019-11-27 11:51:32 +0000

    create apiRouter route

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express")
-const app = express()
 const cors = require("cors")
-
-app.use(cors())
+const app = express()
+const apiRouter = require("./routes/apiRouter.js")
 
 app.use(express.json())
+app.use(cors())
+
+app.use("/api", apiRouter)
 
 module.exports = app
