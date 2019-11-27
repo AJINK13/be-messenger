f4f65f97cb786f52fb15bdf8af8ab47c88eeeed2
aj <ajshanthappa@gmail.com>
2019-11-27 10:53:33 +0000

    initial commit

---
+++ db/data/testdata/users.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    username: "butter_bridge",
+    avatar_url:
+      "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+    online_status: false
+  },
+  {
+    username: "icellusedkars",
+    avatar_url: "https://avatars2.githubusercontent.com/u/24604688?s=460&v=4",
+    online_status: false
+  },
+  {
+    username: "rogersop",
+    avatar_url: "https://avatars2.githubusercontent.com/u/24394918?s=400&v=4",
+    online_status: false
+  },
+  {
+    username: "lurker",
+    avatar_url:
+      "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+    online_status: false
+  }
+]
---
+++ db/seeds/seed.js
@@ -0,0 +1,13 @@
+const { userData } = require("../data/testdata/users.js")
+
+exports.seed = function(knex) {
+  return knex.migrate
+    .rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => {
+      const usersInsertions = knex("users")
+        .insert(userData)
+        .returning("*")
+      return Promise.all([usersInsertions])
+    })
+}
